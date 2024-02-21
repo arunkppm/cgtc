736db92f7f1c2b0266f43a2686b2a5619fd137a7
arunkppm <itsarunkrit@gmail.com>
2024-02-21 10:52:50 +0530

    This something added

---
+++ cgtc1.session.sql
@@ -0,0 +1,15 @@
+--@block create database airbnbfirst;
+use airbnbfirst;
+create table guestinkochi (
+id int auto_increment primary key,
+Name varchar(100),
+Age int,
+Email varchar(255),
+Address varchar(250),
+City varchar(100),
+Country varchar(100)
+);
+
+
+
+
