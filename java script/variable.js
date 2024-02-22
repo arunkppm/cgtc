0f1889f8924b4aa3dfba50b61da1b6150cc5d224
arunkppm <itsarunkrit@gmail.com>
2024-02-22 09:55:53 +0530

    java

---
+++ java script/variable.js
@@ -0,0 +1,8 @@
+let num = 10;
+var num1 = 10;
+const num2 = 10;
+num = 12;
+console.log("This is number:" + num)
+console.log("This is second number:" + num1)
+console.log("This is third number:" + num2)
+console.table(num, num1, num2);
