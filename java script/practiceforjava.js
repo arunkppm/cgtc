117614d4d955fdf1ae9761b8c974046c7aab18a4
arunkppm <itsarunkrit@gmail.com>
2024-02-22 12:28:19 +0530

    practice

---
+++ java script/practiceforjava.js
@@ -0,0 +1,10 @@
+let a= 10;
+let b=20;
+let c=30;
+let d= a+b;
+let e= b*c;
+let f= b/c;
+
+console.log("this is addition:" + d);
+console.log("this is multiplication:" + e);
+console.log("this is division:" + f);
--- java script/variable.js
+++ java script/variable.js
@@ -6,3 +6,11 @@ console.log("This is number:" + num)
 console.log("This is second number:" + num1)
 console.log("This is third number:" + num2)
 console.table(num, num1, num2);
+
+let x= 10;
+let y= 203;
+let z= x+y;
+let m=x-y;
+let d = x/y;
+let Md =x*y;
+console.table(z, m, d, Md);
